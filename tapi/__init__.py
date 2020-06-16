df9cec927c804201e66bfd662c9c86bf45eecdb2
pmaksimov <vur21@ya.ru>
2020-06-16 10:04:01 +0300

    новая версия

--- tapi/__init__.py
+++ tapi/__init__.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 __author__ = 'Pavel Maksimov'
 __email__ = 'vur21@ya.ru'
-__version__ = '2019.12.10'
+__version__ = '2020.06.16'
 
 
 from .adapters import (
